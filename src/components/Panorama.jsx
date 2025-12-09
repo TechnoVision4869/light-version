cccfdc2677eeac696134f4291cffeceae17b6af9
RadwaAhmed4869 <48102202+RadwaAhmed4869@users.noreply.github.com>
2025-12-09 15:21:29 +0200

    typo

--- src/components/Panorama.jsx
+++ src/components/Panorama.jsx
@@ -1,6 +1,6 @@
 import { useState, useMemo, useRef, useEffect, useCallback } from "react";
 import View360, { EquirectProjection } from "@egjs/react-view360";
-import Pin from "./pin";
+import Pin from "./Pin";
 import "@egjs/react-view360/css/view360.min.css";
 
 export default function Panorama({ apartment }) {
