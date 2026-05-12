d9116d26f8bec2948bffb150885fd78b7a0017cc
YoussefAhmed488 <48102202+RadwaAhmed4869@users.noreply.github.com>
2026-05-12 19:25:36 +0300

    Update appConfig.js

--- src/config/appConfig.js
+++ src/config/appConfig.js
@@ -8,7 +8,7 @@ export const APP_CONFIG = {
   // Set to true to use static data, false to use API
   USE_STATIC: false,
   USE_PREDEFINED_POS: true,
-  IDLE_TYPE: ASSET_TYPES.VIDEO,
+  IDLE_TYPE: ASSET_TYPES.IMAGE,
   // Set to true to use panorama hotspots for unit interior, false for single room-interior popup
-  USE_HOTSPOTS: true,
+  USE_HOTSPOTS: false,
 };
