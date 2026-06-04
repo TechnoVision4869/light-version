29df5708e37259e9f9682d61b27f68b4a4b30d2b
RadwaAhmed4869 <48102202+RadwaAhmed4869@users.noreply.github.com>
2026-06-04 16:47:10 +0300

    Update appConfig.js

--- src/config/appConfig.js
+++ src/config/appConfig.js
@@ -2,7 +2,7 @@ import { CONFIG } from "../data/layers";
 
 export const APP_CONFIG = {
   // Set to true to use static data, false to use API
-  USE_STATIC: true,
+  USE_STATIC: false,
   USE_PREDEFINED_POS: CONFIG.USE_PREDEFINED_POS,
   // Set to true to use panorama hotspots for unit interior, false for single room-interior popup
   USE_HOTSPOTS: CONFIG.USE_HOTSPOTS,
