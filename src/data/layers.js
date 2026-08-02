a9fb6d705745a9bdb2016b73fe5b4c99537a94a6
RadwaAhmed4869 <48102202+RadwaAhmed4869@users.noreply.github.com>
2026-08-02 12:48:58 +0300

    Update layers.js

--- src/data/layers.js
+++ src/data/layers.js
@@ -1,7 +1,7 @@
 // import { DEVELOPER_MIX as DEVELOPER } from "./project-mix";
 // import { DEVELOPER_TBK as DEVELOPER, projectPath, config } from "./project-kog";
 import { DEVELOPER_SOMABAY as DEVELOPER, projectPath, config  } from "./project-lighthouse";
-// import { DEVELOPER_SOMABAY as DEVELOPER, projectPath, config  } from "./projectnpm -lightlight";
+// import { DEVELOPER_SOMABAY as DEVELOPER, projectPath, config  } from "./project-lightlight";
 // import { DEVELOPER_SOMABAY as DEVELOPER, projectPath, config } from "./project-sandbox";
 // import { DEVELOPER_SOMABAY as DEVELOPER, projectPath, config } from "./project-demo";
 // import { DEVELOPER_TECHNO as DEVELOPER, projectPath, config } from "./kog-demo";
